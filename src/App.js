cbd9003ac03bd1e660c6494d301c986a5fddc9c8
Dayse Borges <dayseborges030798@gmail.com>
2023-04-25 14:45:23 -0300

    Menu mobile

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Carrousel} from './Componentes/Carrousel/Carrousel';
 import {Welcome} from './Componentes/Welcome/Welcome';
 import {Button} from './Componentes/Button/Button';
+import {Header} from './Componentes/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Carrousel />
       <Welcome />
       <Button />
--- src/Componentes/Welcome/Welcome.jsx
+++ src/Componentes/Welcome/Welcome.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./Welcome.module.css";
+import style from "./styles/Welcome.module.css";
 import logo from "../../assets/images/logo.png";
 
 export const Welcome = () => {
