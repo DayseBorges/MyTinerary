9282ad8e83e004706fc242f49f840ff0157a1a9e
Dayse Borges <dayseborges030798@gmail.com>
2023-04-25 15:28:11 -0300

    Routes ok

--- src/Componentes/Welcome/Welcome.jsx
+++ src/Componentes/Welcome/Welcome.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import style from "./styles/Welcome.module.css";
 import logo from "../../assets/images/logo.png";
+import { TbLocation } from "react-icons/tb";
 
 export const Welcome = () => {
-
   return (
     <>
       <div className={style.containerWelcome}>
+        <div className={style.containerLeft}>
         <h1 className={style.name}>MyTinerary</h1>
         <p className={style.text}>
-          Find your <span className={style.orange}>perfect trip</span>, designed <br />
-          by insiders who know and <span className={style.orange}>love their cities</span> 
+          Find your <span className={style.orange}>perfect trip</span>, designed{" "}
+          <br />
+          by insiders who know and{" "}
+          <span className={style.orange}>love their cities</span>
         </p>
-      </div>
-      <div className={style.imgLogo}>
-        <img src={logo} alt="" />
+        <button className={style.button}>
+          <TbLocation className={style.icon} /> Explore Cities
+        </button>
+        </div>
+          <img className={style.imgLogo} src={logo} alt="" />
       </div>
     </>
   );
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import {Carrousel} from '../../Componentes/Carrousel/Carrousel';
+import {Welcome} from '../../Componentes/Welcome/Welcome';
+
+
+function Home () {
+    return (  
+        <>
+        <Carrousel />
+        <Welcome />
+        </>
+    );
+}
+
+export default Home;
