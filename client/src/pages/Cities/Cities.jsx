2a4b8b1a46e10a7896fb5480aff0f81a71efade1
Ian Camu <iancamu01@hotmail.com>
2023-05-03 09:17:50 -0300

    Filterbar

--- client/src/pages/Cities/Cities.jsx
+++ client/src/pages/Cities/Cities.jsx
@@ -1,8 +1,12 @@
+import Filterbar from '../../Componentes/Filterbar/Filterbar'
+import styles from './styles/Cities.module.css'
+
 const Cities = () => {
     return ( 
-        <>
+        <div className={styles.main}>
+        <Filterbar/>
         <h1>Cities</h1>
-        </>
+        </div>
     );
 }
  
