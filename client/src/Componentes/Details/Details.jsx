baa7ea41ab14484dcfb864f95a22836e5de6f154
Dayse Borges <dayseborges030798@gmail.com>
2023-05-03 11:05:00 -0300

    update

--- client/src/Componentes/Details/Details.jsx
+++ client/src/Componentes/Details/Details.jsx
@@ -1,10 +1,32 @@
 import { useParams } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Details = () => {
     const { id } = useParams()
+    const [city, setCity] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+          try {
+            const res = await axios.get(`http://localhost:3001/api/city/${id}`);
+            setCity(res.data.response);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchData();
+      }, []);
+
     return ( 
-        <h1>Details</h1>
+        <div>
+           
+                <div>
+                    <h1>{city.name}</h1>
+                </div>
+        
+        </div>
     );
 }
- 
+
 export default Details;
